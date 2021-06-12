d4b77ca79e5d4ea7544cc95df12587bc91f15910
pedrooliver13 <pedro@qyon.com>
2021-06-12 10:46:12 -0300

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
+import cors from 'cors';
+
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(routes);
-
-app.get("/", (req, res) => {
-  return res.send("Hello World");
-});
 
 app.listen(3333);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import UserController from "../controllers/UserControllers";
 
 const routes = Router();
 
+routes.get("/", (req, res) => res.send("Hello World"));
 routes.get("/users", UserController.index);
 routes.get("/users/create", UserController.create);
 
